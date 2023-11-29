48c89b6e7624177cb4f53fcda136d6b512ab31d9
johanddc <ivan.peshekhonov@gmail.com>
2023-11-29 22:19:21 +0300

    Add automatic service test

---
+++ test/service_test.py
@@ -0,0 +1,28 @@
+import io
+import requests
+import pandas as pd
+
+from unittest import TestCase
+
+
+class ServiceTest(TestCase):
+    server_url: str = "http://127.0.0.1:8000"
+    data_url: str = "https://raw.githubusercontent.com/hse-mlds/ml/main/hometasks/HT1/cars_train.csv"
+
+    def testPredictItem(self):
+        path = "/predict_item"
+        df = pd.read_csv(self.data_url)
+        item = df.sample(1).to_dict(orient="records")
+        resp = requests.post(self.server_url + path, json=item[0])
+        print(resp.content.decode("utf-8"))
+        assert resp.status_code == 200
+
+    def testPredictItems(self):
+        path = "/predict_items"
+        df = pd.read_csv(self.data_url)
+        df.sample(10).drop(columns="selling_price").to_csv(
+            buffer := io.StringIO(), index=False
+        )
+        resp = requests.post(self.server_url + path, files={"csv": buffer.getvalue()})
+        print(pd.read_csv(io.BytesIO(resp.content)))
+        assert resp.status_code == 200
